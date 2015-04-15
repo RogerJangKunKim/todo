865cc91c975fcb08affc725bb93ce4a4208b49a0
Roger JangKun Kim <rb2stk@gmail.com>
2015-04-15 10:47:12 -0700

    milestone 5. changed connect.php path

--- includes/connect.php
+++ includes/connect.php
@@ -1,8 +1,8 @@
 <?php
 $mysqli = new mysqli('localhost', 'root', 'root', 'todo');
-if (mysqli->connect_erro) {
+if ($mysqli->connect_error) {
 	die('Connect Error (' . $mysqli->connect_errno . ')'
-		. mysqli->connect_error);
+		. $mysqli->connect_error);
 }
 else {
 	echo "Connection made";
