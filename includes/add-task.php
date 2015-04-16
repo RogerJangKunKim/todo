ecbd9a34afa2ec5d1ec9a3e65f52fb2f0c8863ac
Roger JangKun Kim <rb2stk@gmail.com>
2015-04-16 11:07:27 -0700

    Milestone 12

--- includes/add-task.php
+++ includes/add-task.php
@@ -1,1 +1,21 @@
-add-task.php
+<?php
+	$task = strip_tags($_POST['task']);
+	$date = date('Y-m-d');
+	$time = date('H:i:s');
+
+	include('connect.php');
+
+	$mysqli = new mysqli('localhost', 'root', 'root', 'todo');
+	$mysqli -> query("INSERT INTO tasks VALUES ('', '$task', '$date', '$time')");
+
+	$query = "SELECT * FROM tasks WHERE task = '$task' and date = '$date' and time = '$time'";	
+
+	if ($result = $mysqli->query($query)) {
+		while ($row = $result->fetch_assoc()){
+			$task_id = $row['id'];
+			$task_name = $row['task'];
+		}
+	}
+
+	$mysqli->close();
+?>
